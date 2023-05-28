0d99a89c11f8c79176cb7b93fd27c5311bfadaa9
crebs86 <crebs.m@gmail.com>
2023-05-28 20:08:54 -0300

    Estrutura inicial de pastas e arquivos.

---
+++ app/Frota/Controllers/CarrosController.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Traits\ACL;
+use App\Traits\Helpers;
+use App\Http\Controllers\Controller;
+
+
+class CarrosController extends Controller
+{
+    use ACL, Helpers;
+}
---
+++ app/Frota/Controllers/MotoristasController.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Traits\ACL;
+use App\Traits\Helpers;
+use App\Http\Controllers\Controller;
+
+
+class MotoristaController extends Controller
+{
+    use ACL, Helpers;
+}
